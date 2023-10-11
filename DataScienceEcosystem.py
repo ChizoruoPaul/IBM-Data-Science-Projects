501ef2b719aab533dc11dcf15726622661e6c2c6
ChizoruoPaul <147660797+ChizoruoPaul@users.noreply.github.com>
2023-10-11 21:03:25 +0100

    Data Science Ecosystem
    These are my initial projects for the IBM data science certification

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem
+
+# In this notebook, Data Science Tools and Ecosystem are summarized.
+
+# Some of the popular languages that Data Scientists use are:
+# 1. Python
+# 2. R
+# 3. SQL
+# 4. Julia
+
+# Some of the commonly used libraries used by Data Scientists include:
+# 
+# 1. NumPy
+# 2. Pandas
+# 3. Tensorflow
+# 4. Keras
+# 5. ggplot
+# 6. Matplotlib
+# 
+
+# | Data Science Tools |
+# |--------------------|
+# | Jupyter Notebook   |
+# | Jupyter Lab        |
+# | Visual Studio      |
+# | Rstudio            |
+
+# # Introducing arithmetic expression examples
+# 
+# 
+# Below are a few examples of evaluating arithmetic expressions in Python
+
+# In[4]:
+
+
+# This a simple arithmetic expression to mutiply then add integers
+
+(3*4)+5
+
+
+# In[5]:
+
+
+# To Convert 200 minute =s into Hours
+
+a = 200/60
+
+print ("The hours is", a)
+
+
+# ## Arthur
+# 
+# Chizoruo Paul Udoeze
+
+# In[ ]:
+
+
+
+
